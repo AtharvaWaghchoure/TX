ae142eecbac97db34bd42e8bb5aee24ab4054edf
AtharvaWaghchoure <atharval18wagh@gmail.com>
2023-07-23 01:22:59 +0530

    everything else

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import WalletAddress from "./WalletAddress";
+
+function App() {
+  return (
+    <>
+      <WalletAddress />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
+        sit nulla? Repellat quo deserunt voluptatem, sed recusandae dolorum
+        voluptates, non, numquam dolorem magnam autem? Corporis eos suscipit ab
+        assumenda inventore.
+      </p>
+    </>
+  );
+}
+
+export default App;
